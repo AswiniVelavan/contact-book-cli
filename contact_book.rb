8d694f7aca3bfcfd57baa9f172e9080114c712c8
AswiniVelavan <velavanaswini@gmail.com>
2025-08-10 01:11:56 +0000

    view_commit

--- contact_book.rb
+++ contact_book.rb
@@ -9,21 +9,19 @@ class ContactBook
     loop do
       puts "Welcome to Contact Book app!"
       puts "1. Add Contact"
-      puts "2. Delete Contact"
-      puts "3. Edit Contact"
-      puts "4. View Contacts"
-      puts "5. Exit"
+      puts "2. View Contacts"
+      puts "3. Exit"
+      
       puts "Please Choose an Option"
       option = gets.chomp.to_i
 
       case option
         when 1
           add_contact
-        when 3 
-          edit_contact
-        when 3
+        when 2
           view_contacts
-        when 4
+        when 3         
+          
           puts " Thank You for visting Contact Book app have a great day! "
           break
         else
@@ -43,27 +41,12 @@ class ContactBook
       phone_number = gets.chomp
       puts "enter the email address "
       email = gets.chomp
-      @contact_book << Contact.new(first_name, last_name, phone_number, email)
+      @contact_book << Contact.new(name, phone_number, email)
       puts "contacts added sucessfully"
     end
-
-    def delete_contact
-      puts  "Enter the First name "
-      name_delte = gets.chomp
-      puts "Enter Phone number"
-      phone_number_delete = gets.chomp
-      puts "Enter the email"
-      email_delete =gets.chomp
-
-      
-        end
-
-
-    end
+        
     def view_contacts
-      puts "The contact list \n #{@contact_book}"
-    end
-
-
-  
+      puts "The contacts in the list"
+      @contact_book
+    end  
 end
